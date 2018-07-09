18b52ffdc6c5246498a118af9fe9fab7a364234c
corsa144 <40776473+corsa144@users.noreply.github.com>
2018-07-09 17:33:23 -0300

    Recuperatorio
    ejercicios 1 y 3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,20 @@
 function mostrar()
 {
+	var descuento;
+	var precio;
+	var precioConDescuento;
+	var valorDelDescuento;
+	var iva;
+	var precioFinal;
 
+	precio=prompt("el precio");
+	descuento=prompt("descuento");
+	valorDelDescuento=( precio*descuento / 100);
+	precioConDescuento=( precio- valorDelDescuento);
+	iva=(precioConDescuento*21/100);
+	precioFinal=(precioConDescuento+iva);
+
+	alert("el descuento es de "+valorDelDescuento+"entonces queda "+precioConDescuento+"mas iva "+iva);
+
+	document.getElementById('elPrecioFinal').value=precioFinal;
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,20 @@
 
 function mostrar()
 {
+	var base;
+	var altura;
+	var resultadoSuperficie;
+	var resultadoPerimetro;
+
+	base=document.getElementById('base').value;
+	base=parseInt(base);
+	altura=document.getElementById('altura').value;
+	altura=parseInt(altura);
+
+	resultadoSuperficie=base*altura/2;
+	resultadoPerimetro=base*3;
+
+	alert('El area es '+resultadoSuperficie+'\nEl perimetro es '+resultadoPerimetro);
+
 
 }
