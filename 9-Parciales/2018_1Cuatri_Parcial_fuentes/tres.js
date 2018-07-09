f28d169fb934d8bf131d2059a16bab7ff1105163
corsa144 <40776473+corsa144@users.noreply.github.com>
2018-07-09 18:54:04 -0300

    primer parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,14 @@
 function mostrar()
 {
+	var precio;
+	var descuento;
+	var precioFinal;
 
+	precio=prompt("ingrese el precio");
+	descuento=prompt("ingrese el porcentaje de descuento");
+	precioFinal=(precio - precio*descuento/100);
+
+	//console.log(precioFinal);
+	document.getElementById('elPrecioFinal').value=precioFinal;
+	//console.log(precioFinal);
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,11 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
 
+	ancho=prompt("ingrese el ancho");
+	largo=prompt("ingrese el largo");
+
+	alert(ancho*2+largo*2+" centimetros ");
 }
