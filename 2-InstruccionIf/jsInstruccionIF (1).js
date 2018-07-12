e3c5472f5df951276be58334a8645fc355f7748d
corsa144 <40776473+corsa144@users.noreply.github.com>
2018-07-12 20:46:58 -0300

    ejercicio uno
    no me sale

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,30 @@
 function mostrar()
-{
 //tomo la edad  
+{
 
+	if(edad==15)
 
+	
 
-}//FIN DE LA FUNCIÓN
+		{	
+			var edad;
+			
+			edad=document.getElementById("niña bonita").value;
+			edad=parseInt("ingrese su edad");
+
+			alert(edad);
+		
+
+		}
+
+	}//else
+	//{
+		//"error"
+//}//
+
+		
+
+//FIN DE LA FUNCIÓN
+/*if(condicion:true or false){}edad==18(igualdad) !true==false >(mayor) >= <(menor) <= !=(desigual);
+ igualdad estricta=== number==stream(2=="2") estos son los operadores que estan en la condicion;
+  &&(and) ||(or) else¨{}por cada if va un else que es un entonces
