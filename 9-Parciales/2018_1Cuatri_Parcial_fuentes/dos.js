9492d086d60175b3dab3b7ba0f5e0bbf798665e8
corsa144 <40776473+corsa144@users.noreply.github.com>
2018-07-12 20:53:38 -0300

    segundo ejercicio del parcial
    creo que bien hecho

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,10 @@
 function mostrar()
 {
-  
+  	var usuario;
+  	var localidad;
+
+  	usuario=document.getElementById('elNombre').value;
+  	localidad=document.getElementById('laLocalidad').value;
+
+  	alert("usted es " + usuario + " vive en la localidad de " + localidad);
 }
