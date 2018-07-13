f2fda546f0b7303df8463b7c69af40b68b41fea2
corsa144 <40776473+corsa144@users.noreply.github.com>
2018-07-12 21:31:40 -0300

    copiados del profe
    entender

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,15 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var nota;
+	nota=Math.floor(Math.random()*(11-1)+1);
 	
 
 }//FIN DE LA FUNCIÓN
+
+{
+	if(nota>=9)
+	{
+		alert()
+	}elseif(nota>=4){}else(nota)
+}
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,14 @@
 function mostrar()
-{
+//{
 	//Genero el número RANDOM entre 1 y 10 
-	
+	/*var num;
 
-}//FIN DE LA FUNCIÓN
+	num=math.floor(math.random()*(max-min))+min;
+
+}//FIN DE LA FUNCIÓN*/
+
+{
+	var num;
+	num=Math.floor(Math.random()*(11-1)+1);
+	alert(num);
+}
