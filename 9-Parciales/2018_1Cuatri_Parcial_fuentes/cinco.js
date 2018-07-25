29738b54f6dc5e2af043526bc641ecfaa8fbdb15
corsa144 <40776473+corsa144@users.noreply.github.com>
2018-07-24 22:35:09 -0300

    parcial
    4 y 5

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,27 @@
 function mostrar()
 {
+	var planeta;
+
+	planeta=prompt("ingrese planeta");
+
+	switch(planeta)
+	{
+		case "mercurio":
+		case "venus":
+			alert("acá hace más calor");
+			break;
+		case "tierra":
+			alert("acá vivimos");
+			break;
+		case "marte":
+		case "jupiter":
+		case "saturno":
+		case "neptuno":
+		case "urano":
+			alert("acá hace más frio");
+			break;
+		default:
+			alert("no es un planeta valido");
+	}
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,28 @@
 function mostrar()
 {
+	var numeroUno,numeroDos;
+
+	numeroUno=prompt("ingrese numero 1");
+	numeroUno=parseInt(numeroUno);
+
+	numeroDos=prompt("ingrese numero 2");
+	numeroDos=parseInt(numeroDos);
+
+	if(numeroUno==numeroDos)
+	{
+		alert(numeroUno+""+numeroDos);
+	}
+	else if(numeroUno>numeroDos)
+	{
+		alert(numeroUno-numeroDos);
+	}
+	else 
+	{
+		alert(numeroUno+numeroDos);
+		if(numeroUno+numeroDos>10)
+		{
+			alert("la suma es "+(numeroUno+numeroDos)+" y supero el 10");
+		}
+	}
 
 }
