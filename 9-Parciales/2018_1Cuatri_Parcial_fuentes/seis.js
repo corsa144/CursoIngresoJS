bdb3854d97f6197a6e77d822afb6af9d2595aadd
corsa144 <40776473+corsa144@users.noreply.github.com>
2018-07-30 20:02:35 -0300

    parcial
    ejercicio 6

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
@@ -1,4 +1,46 @@
 function mostrar()
 {
+	var laHora;
 
+	laHora=document.getElementById('laHora').value;
+	laHora=parseInt(laHora);
+
+		switch(laHora)
+		{
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			case 10:
+			case 11:
+				alert("es de mañana");
+				break;
+			case 12:
+			case 13:
+			case 14:
+			case 15:
+			case 16:
+			case 17:
+			case 18:
+			case 19:
+				alert("es de tarde");
+				break;
+			case 20:
+			case 21:
+			case 22:
+			case 23:
+				alert("es de noche, ¡a dormir!");
+				break;
+			case 24:
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+				alert("es de noche");
+				break;
+
+			default:
+				alert("la hora no es valida");
+		}
 }
